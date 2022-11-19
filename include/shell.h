9e1e9f4fce8d5d1afee641e2cf513c2fd21fe775
Hocine <ocineh@hotmail.com>
2022-11-19 09:21:00 +0100

    Add the color enum

--- include/shell.h
+++ include/shell.h
@@ -1,4 +1,19 @@
 #ifndef SHELL_H
 #define SHELL_H
 
+typedef enum {
+	GREEN,
+	RED,
+	BLUE,
+	CYAN,
+	YELLOW, // or brown
+	WHITE, // or gray
+	BLACK,
+	PURPLE,
+} color;
+
+char* to_fg_color(color c);
+
+char* to_bg_color(color c);
+
 #endif //SHELL_H
